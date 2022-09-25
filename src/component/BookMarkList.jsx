cff7fe02c12d25b52ee192abdbe6342350ccf3a7
lky <skyvip2633@gmail.com>
2022-09-25 17:02:02 +0900

    first upload

--- src/component/BookMarkList.jsx
+++ src/component/BookMarkList.jsx
@@ -30,7 +30,7 @@ export default function BookMarkList() {
 
     const [data, setData] = useState([]);
     useLayoutEffect(() => {
-        fetch(`https://book-marking.herokuapp.com/api/users`)
+        fetch(`https://book-marking.herokuapp.com/users`)
             .then(res => {
                 return res.json()
             })
--- src/component/Comment.jsx
+++ src/component/Comment.jsx
@@ -60,7 +60,7 @@ export default function Comment() {
         setTitle("");
         setLink("");
 
-        fetch(`https://book-marking.herokuapp.com/api/comments/`, {
+        fetch(`https://book-marking.herokuapp.com/comments/`, {
             method: "POST",
             headers: {
                 "Content-Type": 'application/json; charset=UTF-8',
@@ -107,7 +107,7 @@ export default function Comment() {
     }
 
     useLayoutEffect(() => {
-        fetch(`https://book-marking.herokuapp.com/api/users`)
+        fetch(`https://book-marking.herokuapp.com/users`)
             .then(res => {
                 return res.json()
             })
