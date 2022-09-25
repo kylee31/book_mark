e1dec48362e26b57f312a9ddc8770bdb815e9197
lky <skyvip2633@gmail.com>
2022-09-25 16:17:20 +0900

    deploy

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("./db.json");
+const router = jsonServer.router("./bookmark.json");
 const middlewares = jsonServer.defaults({
     static: "./build",
 });
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3001;
 
 server.use(middlewares);
 server.use(
