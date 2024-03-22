af68694d8d7c5f89c1f9496c5f52d8d725c50a1c
lky <skyvip2633@gmail.com>
2024-03-22 18:28:04 +0900

    [feat] 이미지 수정

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import AddCate from './pages/AddCate';
 import MainPage from "./pages/MainPage";
 import WrongPage from "./pages/WrongPage";
 import Login from "./pages/Login";
-import Layout from "./util/Layout";
+import Layout from "./styles/Layout";
 import PrivateRoute from "./util/PrivateRoute";
 
 export default function App() {
@@ -17,7 +17,7 @@ export default function App() {
           <Route path="/main" element={<PrivateRoute><MainPage /></PrivateRoute>} />
           <Route path="/cate/:name" element={<PrivateRoute><MyCate /></PrivateRoute>} />
           <Route path="/createcate" element={<PrivateRoute><AddCate /></PrivateRoute>} />
-          <Route path="/*" element={<WrongPage />} />
+          <Route path="/main/*" element={<WrongPage />} />
         </Routes>
       </Layout>
     </BrowserRouter>
