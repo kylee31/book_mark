ef20b480f06eece10be420f26aa5d323d8069249
lky <skyvip2633@gmail.com>
2024-03-23 01:53:15 +0900

    [feat] refresh 로그인 페이지로 복귀하는 error 수정

--- src/util/isLogin.js
+++ src/util/isLogin.js
@@ -1,6 +1,10 @@
+//authService.currentUser 사용 시 새로고침마다 Firebase는 사용자 인증 상태를 다시 확인하고, 
+//이 프로세스가 완료되기 전에는 currentUser가 null로 설정되어 로그인 페이지로 돌아가게 됨
+
 import { authService } from "../fbase";
 
-export const isLogin = () => !!authService.currentUser;
+//로그인 시 access Token 저장하여 Token으로 인가하기
+export const isLogin = () => !!(localStorage.getItem('token') || authService.currentUser);
 
 /*콜백 함수 반환
 import { onAuthStateChanged } from "firebase/auth";
